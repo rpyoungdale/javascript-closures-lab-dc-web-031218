d30f6c594cd7409a5dec2a6b12092cc357579aa4
rpyoungdale <youngdale95@gmail.com>
2018-04-26 13:58:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+const bumpCounter = function () {
+  let counter = 0;
+
+  return {
+    addBump: function () {
+      return counter+=1;
+    },
+
+    getBumps: function () {
+      return counter;
+    }
+  };
+};
+
+const createAnimal = function (animalType) {
+  return function(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+};
+
+const sharkCreator = createAnimal("Shark");
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+const sharkWithFrickinCannon = sharkCreator("Cannon");
